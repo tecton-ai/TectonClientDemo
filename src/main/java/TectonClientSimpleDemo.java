a4f5a4946e8a2fce8ca583c63cb53775bbc4b6d1
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2022-07-05 07:45:30 -0700

    Add sample demo

--- src/main/java/TectonClientSimpleDemo.java
+++ src/main/java/TectonClientSimpleDemo.java
@@ -1,8 +1,11 @@
 import com.tecton.client.TectonClient;
 import com.tecton.client.model.FeatureValue;
+import com.tecton.client.model.ValueType;
 import com.tecton.client.request.GetFeaturesRequest;
 import com.tecton.client.request.GetFeaturesRequestData;
 import com.tecton.client.response.GetFeaturesResponse;
+
+import java.time.Duration;
 import java.util.Map;
 
 public class TectonClientSimpleDemo {
@@ -29,7 +32,18 @@ public class TectonClientSimpleDemo {
     // Send request and receive response
     GetFeaturesResponse getFeaturesResponse = tectonClient.getFeatures(getFeaturesRequest);
 
+    //Network + Online Store Latency
+    Duration latency = getFeaturesResponse.getRequestLatency();
+
+    //Get Feature Vector
     //List<FeatureValue> featureValues = getFeaturesResponse.getFeatureValues();
     Map<String, FeatureValue> featureValues = getFeaturesResponse.getFeatureValuesAsMap();
+
+    //Access Feature Name and value
+    FeatureValue sampleFeatureValue = featureValues.get("user_transaction_amount_metrics.amt_mean_1d_10m");
+    String featureNamespace = sampleFeatureValue.getFeatureNamespace();
+    String featureName = sampleFeatureValue.getFeatureName();
+    ValueType valueType = sampleFeatureValue.getValueType();
+    Double value = sampleFeatureValue.float64Value();
   }
 }
